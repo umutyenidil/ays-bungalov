dbcf31e5097124651b4de2214a21a3ca6ad6fa0b
Umut Yenidil <umut.yenidil9@gmail.com>
2024-05-31 21:01:05 +0300

    update(): yeni resimler eklendi

--- src/views/components/carousels/Home.Carousel.jsx
+++ src/views/components/carousels/Home.Carousel.jsx
@@ -1,18 +1,24 @@
 import {Carousel} from "primereact/carousel";
 import HomeCarouselItem from "../items/HomeCarousel.Item.jsx";
 import image1 from '../../../assets/images/image1.jpg';
-import image2 from '../../../assets/images/image2.jpg';
 import image3 from '../../../assets/images/image3.jpg';
 import image4 from '../../../assets/images/image4.jpg';
 import image5 from '../../../assets/images/image5.jpg';
+import image10 from '../../../assets/images/10.jpg';
+import image11 from '../../../assets/images/11.jpg';
+import image12 from '../../../assets/images/12.jpg';
+import image13 from '../../../assets/images/13.jpg';
+import image14 from '../../../assets/images/14.jpg';
+import image15 from '../../../assets/images/15.jpg';
+import image16 from '../../../assets/images/16.jpg';
+import image17 from '../../../assets/images/17.jpg';
+import image18 from '../../../assets/images/18.jpg';
+import image19 from '../../../assets/images/19.jpg';
 
 const HomeCarousel = () => {
     const items = [
         {
             image: image1,
-        },
-        {
-            image: image2,
         },
         {
             image: image3,
@@ -22,6 +28,36 @@ const HomeCarousel = () => {
         },
         {
             image: image5,
+        },
+        {
+            image: image10,
+        },
+        {
+            image: image11,
+        },
+        {
+            image: image12,
+        },
+        {
+            image: image13,
+        },
+        {
+            image: image14,
+        },
+        {
+            image: image15,
+        },
+        {
+            image: image16,
+        },
+        {
+            image: image17,
+        },
+        {
+            image: image18,
+        },
+        {
+            image: image19,
         }
     ];
 
--- src/views/components/galleries/Home.Gallery.jsx
+++ src/views/components/galleries/Home.Gallery.jsx
@@ -1,16 +1,34 @@
 import image1 from '../../../assets/images/image1.jpg';
-import image2 from '../../../assets/images/image2.jpg';
 import image3 from '../../../assets/images/image3.jpg';
 import image4 from '../../../assets/images/image4.jpg';
 import image5 from '../../../assets/images/image5.jpg';
+import image10 from '../../../assets/images/10.jpg';
+import image11 from '../../../assets/images/11.jpg';
+import image12 from '../../../assets/images/12.jpg';
+import image13 from '../../../assets/images/13.jpg';
+import image14 from '../../../assets/images/14.jpg';
+import image15 from '../../../assets/images/15.jpg';
+import image16 from '../../../assets/images/16.jpg';
+import image17 from '../../../assets/images/17.jpg';
+import image18 from '../../../assets/images/18.jpg';
+import image19 from '../../../assets/images/19.jpg';
 
 const HomeGallery = () => {
     const sources = [
         image1,
-        image2,
         image3,
         image4,
         image5,
+        image10,
+        image11,
+        image12,
+        image13,
+        image14,
+        image15,
+        image16,
+        image17,
+        image18,
+        image19,
     ];
 
     return (
